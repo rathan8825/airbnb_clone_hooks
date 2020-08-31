dc8102cc10252eaced1a38e078864a1bcad21988
Rathan Kumar Chikkam <38204857+rathan8825@users.noreply.github.com>
2020-08-31 15:27:45 +0200

    search refactored

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Search.css';
 import '../Home/Home.css';
 import Spinner from '../../utility/Spinner/Spinner';
@@ -7,30 +7,48 @@ import Cities from '../../utility/City/Cities';
 import Activities from '../../utility/Activity/Activities';
 import Venues from '../../utility/Venue/Venues';
 
-class Search extends Component{
+function Search(props){
 
-    state = {
-        activities: [],
-        cities: [],
-        venues: [],
-        apiResponse: false,
-    }
+    const [ activities, setActivities ] = useState([]);
+    const [ cities, setCities ] = useState([]);
+    const [ venues, setVenues ] = useState([]);
+    const [ apiResponse, setResponse ] = useState(false);
+ 
+    // state = {
+    //     activities: [],
+    //     cities: [],
+    //     venues: [],
+    //     apiResponse: false,
+    // }
 
-    async componentDidMount(){
-        const searchTerm = this.props.match.params.searchTerm;
-        const url = `${window.apiHost}/search/${searchTerm}`;
-        const resp = await axios.get(url);
-        console.log(resp.data);
-        this.setState({
-            activities: resp.data.activities,
-            cities: resp.data.cities,
-            venues: resp.data.venues,
-            apiResponse: true,
-        })
-    }
+    useEffect(() => {
+        const fetchSearchData = async() => {
+            const searchTerm = props.match.params.searchTerm;
+            const url = `${window.apiHost}/search/${searchTerm}`;
+            const resp = await axios.get(url);
+            setActivities(resp.data.activities);
+            setCities(resp.data.cities);
+            setVenues(resp.data.venues);
+            setResponse(true);
+        }
+        fetchSearchData()
+    },[])
 
-    render(){
-        if(!this.state.apiResponse){
+    // async componentDidMount(){
+    //     const searchTerm = this.props.match.params.searchTerm;
+    //     const url = `${window.apiHost}/search/${searchTerm}`;
+    //     const resp = await axios.get(url);
+    //     console.log(resp.data);
+    //     this.setState({
+    //         activities: resp.data.activities,
+    //         cities: resp.data.cities,
+    //         venues: resp.data.venues,
+    //         apiResponse: true,
+    //     })
+    // }
+
+
+        if(!apiResponse){
             return <Spinner />
         }
 
@@ -38,19 +56,19 @@ class Search extends Component{
             <div className="container-fluid lower-fold">
                 <div className="row">
                     <div className="col s12">
-                        <Cities cities={this.state.cities} header="Cities Matching Your Search" />
+                        <Cities cities={cities} header="Cities Matching Your Search" />
                     </div>            
                     <div className="col s12">
-                        <Activities activities={this.state.activities} header="Activies Matching Your Search" />
+                        <Activities activities={activities} header="Activies Matching Your Search" />
                     </div>            
                     <div className="col s12">
-                        <Venues venues={this.state.venues} header="Venues matching your search" />
+                        <Venues venues={venues} header="Venues matching your search" />
                     </div>            
 
                 </div>
             </div>
         )
     }
-}
+
 
 export default Search;
